6c1f1ecfcf0629ee750c1dbf18f2c1c5448e999d
jobimax <johnchaiyasarikul@gmail.com>
2014-12-18 23:26:33 -0500

    added user profile pic endpoint

--- server/server.js
+++ server/server.js
@@ -1,38 +1,14 @@
-var express = require('express');
-var http = require('http');
-var url = require("url");
-var util = require('util');
-var app = express();
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app     = express();
 
-//var client = redis.createClient();
+var SDBcontroller = require('./Controllers/SDBController'),
+    MDBcontroller = require('./Controllers/MDBController'),
+    S3controller  = require('./Controllers/S3Controller');
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-});
-
-app.use(bodyParser());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.engine('html', require('ejs').renderFile);
-app.use("/javascript", express.static(__dirname + '/public/js'));
-app.use("/images", express.static(__dirname + '/public/images'));
-app.use("/views", express.static(__dirname + '/views'));
-app.use("/semantic", express.static(__dirname + '/public/semantic'));
-
-app.use(multer({dest: './uploads/'}));
-
-app.set('view engine', 'html');
-
-//loginAPI
-app.get('/login', function(req, res){
-    var params = url.parse(req.url, true).query;
-    console.log(params);
-});
-
+// app.post('/login',SDBcontroller.login);
+// app.post('/checkin',SDBcontroller.checkin);
+app.get('/listFollowers/:email',MDBcontroller.listFollowers);
+app.get('/getUserPic/:email',S3controller.getUserPic);
 
 var server = app.listen(2015, function() {
     console.log('Server is working!');
