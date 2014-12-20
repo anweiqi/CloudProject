1e225b195f66b98c3deacefaaacc76e76c96e256
jobimax <johnchaiyasarikul@gmail.com>
2014-12-20 18:21:45 -0500

    implemented testing for picture upload to s3

--- server/Controllers/S3Controller.js
+++ server/Controllers/S3Controller.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('awscredentials');
 var s3 = new AWS.S3();
-var s3Bucket = new AWS.S3( { params: {Bucket: 'myBucket'} } );
+var s3Bucket = new AWS.S3( { params: {Bucket: 'userpicscloud'} } );
 
 module.exports.getUserPic = function(req,res){
 	var email = req.params.email;
@@ -13,8 +13,10 @@ module.exports.getUserPic = function(req,res){
 	})
 }
 module.exports.postUserPic = function(req,res){
-	var email = req.params.email;
-	var data = {Key: email, Body: req.body.image};
+	//var email = req.params.email;
+	console.log(req);
+	var data = {Key: req.params.email, Body: req.body};
+	console.log(data);
 	s3Bucket.putObject(data, function(err, data){
     	if (err) console.log('Error uploading data: ', data); 
     	else console.log('succesfully uploaded the image!');
--- server/server.js
+++ server/server.js
@@ -5,11 +5,53 @@ var SDBcontroller = require('./Controllers/SDBController'),
     MDBcontroller = require('./Controllers/MDBController'),
     S3controller  = require('./Controllers/S3Controller');
 
+var fs = require('fs');
+fs.readFile('test.jpg', function(err, data) {
+	request = {
+		params: {email:"cool.jpg"},
+		body:data
+	};
+	S3controller.postUserPic(request);
+});
+
 // app.post('/login',SDBcontroller.login);
 // app.post('/checkin',SDBcontroller.checkin);
 app.get('/listFollowers/:email',MDBcontroller.listFollowers);
 app.get('/getUserPic/:email',S3controller.getUserPic);
-app.post('/getUserPic/:email',S3controller.getUserPic);
+app.get('/test',function(req,res) {
+	var test = [
+		{
+			color: "red",
+			value: "#f00"
+		},
+		{
+			color: "green",
+			value: "#0f0"
+		},
+		{
+			color: "blue",
+			value: "#00f"
+		},
+		{
+			color: "cyan",
+			value: "#0ff"
+		},
+		{
+			color: "magenta",
+			value: "#f0f"
+		},
+		{
+			color: "yellow",
+			value: "#ff0"
+		},
+		{
+			color: "black",
+			value: "#000"
+		}
+	];
+	res.send(test);
+});
+app.post('/postUserPic',S3controller.postUserPic);
 var server = app.listen(2015, function() {
     console.log('Server is working!');
 });
