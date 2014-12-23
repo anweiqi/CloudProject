8b0faf3df1ff26d16fd68aee291b6d1e503d9bb1
Jiuyang Zhao <jiuyangzhao@dyn-160-39-221-151.dyn.columbia.edu>
2014-12-23 12:07:13 -0500

    runnable

--- iOSCloudProject/iOSCloudProject/LoginViewController.m
+++ iOSCloudProject/iOSCloudProject/LoginViewController.m
@@ -57,9 +57,11 @@ static NSString * const kClientId = @"814816072679-6hfkd2q1n8b8slh21e0uie5ctgt0g
 
 - (void) myLoginTapped{
     NSLog(@"%@",_loginView.emailTextField.text);
+    NSLog(@"The PW is !!!%@", _loginView.passwordTextField.text);
     
     NSURLComponents *components = [NSURLComponents componentsWithString:@"http://160.39.221.6:2015/login"];
     NSDictionary *queryDictionary = @{ @"email": _loginView.emailTextField.text, @"password": _loginView.passwordTextField.text};
+    
     NSMutableArray *queryItems = [NSMutableArray array];
     for (NSString *key in queryDictionary) {
         [queryItems addObject:[NSURLQueryItem queryItemWithName:key value:queryDictionary[key]]];
